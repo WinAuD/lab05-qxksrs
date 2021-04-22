e5739dd27a1458758ce566a6ef6a59c728c2a166
WinAuD <mailchris1@gmx.de>
2021-04-22 09:30:57 +0200

    3 files modified

--- src/WikiFetch.js
+++ src/WikiFetch.js
@@ -2,9 +2,69 @@ import React, { Component } from "react";
 import './WikiFetch.css';
 
 class WikiFetch extends Component {
+
+  state = { text : " --- "};
+
+  handleChange = (event) => {
+     //alert(event.target.value);
+     this.fetchData();
+  }   
+
+  fetchData() {
+    // 1. Create a new XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
+
+    // 2. Configure: <Method> , <URL>
+    xhr.open('GET', 'https://de.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&format=json&titles=a');
+
+    // 3. Send the request to the destination
+    xhr.send();
+
+    // 4. Callback after the response is received
+    xhr.onload = () => {
+      if (xhr.status !== 200) { // analyze HTTP status of the response
+        alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+      } else { // show the result
+        //alert("Received: " + xhr.responseText);
+      }
+
+      xhr.onprogress = function (event) {
+        if (event.lengthComputable) {
+          alert(`Received ${event.loaded} of ${event.total} bytes`);
+        } else {
+          alert(`Received ${event.loaded} bytes`); // no Content-Length
+        }
+      };
+
+      xhr.onerror = function () {
+        alert("Request failed");
+      };
+
+      //const str = JSON.parse(xhr.responseText);
+      //this.setState({ text: str.ip });
+
+      this.setState({text: this.getTextFromQuery(xhr.responseText)})
+    };
+  }
+
+  getTextFromQuery(xhrResponseText) {
+    let str = "";
+    const myObj = JSON.parse(xhrResponseText);
+    if (myObj.query === undefined) {
+      str = "---"
+    } else {
+      const keyVal = Object.keys(myObj.query.pages);
+      str = myObj.query.pages[keyVal].extract;
+    }
+    return str;
+  }
+
   render() {
     return (
-      <h1>Hello, my world!</h1>
+      <div>
+        <input className="NiceInput" type="text" onChange={this.handleChange}/>
+        <p className="NiceOutput">"Received: " {this.state.text}</p>
+      </div>
     );
   }
 }
