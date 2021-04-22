2d62b74342d5d7fad04c26562dfe4109a280d0a1
WinAuD <mailchris1@gmx.de>
2021-04-22 08:32:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lab05-qxksrs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/lab05-qxksrs)
---
+++ src/WikiFetch.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './WikiFetch.css';
+
+class WikiFetch extends Component {
+  render() {
+    return (
+      <h1>Hello, my world!</h1>
+    );
+  }
+}
+
+export default WikiFetch;
